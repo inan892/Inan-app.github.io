9b4a1e1363f1653361a55a276ef4837ef8ed757b
inan892 <inanulasozenc@gmail.com>
2025-02-23 18:02:38 +0300

    Update anasayfa.js

--- anasayfa.js
+++ anasayfa.js
@@ -4,8 +4,22 @@ window.addEventListener('load', function() {
     if (loggedInUser) {
         // Eğer kullanıcı giriş yaptıysa, hoş geldiniz mesajı göster
         document.getElementById('welcomeMessage').textContent = `Hoş geldiniz, ${loggedInUser}!`;
+
+        // Operatör kontrolü
+        if (loggedInUser === "uras" || loggedInUser === "inan") {
+            document.getElementById('assignment1').contentEditable = true;
+            document.getElementById('assignment2').contentEditable = true;
+            document.getElementById('assignment3').contentEditable = true;
+        }
     } else {
         // Eğer giriş yapılmadıysa, giriş sayfasına yönlendir
-        window.location.href = 'login.html';
+        window.location.href = 'index.html';
     }
+});
+// Gece modu butonunu seç
+const nightModeButton = document.getElementById('nightModeButton');
+
+// Gece modunu başlatmak ve durdurmak için gerekli işlev
+nightModeButton.addEventListener('click', function() {
+    document.body.classList.toggle('night-mode'); // Gece modunu aç veya kapat
 });
